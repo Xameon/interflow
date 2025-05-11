0d32fbbde77e29a546f14e45f17857bd91f15328
Xameon <andrej.shmalenko@gmail.com>
2025-05-11 13:57:57 +0300

    Integreted Firebase & Images uploader.

---
+++ src/components/ImagesUploader.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Box, FileUpload, FileUploadRootProps, Icon } from '@chakra-ui/react';
+import { LuUpload } from 'react-icons/lu';
+
+export const ImagesUploader = ({ ...props }: FileUploadRootProps) => {
+  return (
+    <FileUpload.Root {...props}>
+      <FileUpload.HiddenInput />
+      <FileUpload.Dropzone>
+        <Icon size='md' color='fg.muted'>
+          <LuUpload />
+        </Icon>
+        <FileUpload.DropzoneContent>
+          <Box>Drag and drop files here</Box>
+          <Box color='fg.muted'>.png, .jpg up to 5MB</Box>
+        </FileUpload.DropzoneContent>
+      </FileUpload.Dropzone>
+      <FileUpload.ItemGroup>
+        <FileUpload.Context>
+          {({ acceptedFiles }) =>
+            acceptedFiles.map(file => (
+              <FileUpload.Item key={file.name} file={file}>
+                <FileUpload.ItemPreviewImage h='5rem' w='5rem' />
+                <FileUpload.ItemName />
+                <FileUpload.ItemSizeText />
+                <FileUpload.ItemDeleteTrigger />
+              </FileUpload.Item>
+            ))
+          }
+        </FileUpload.Context>
+      </FileUpload.ItemGroup>
+    </FileUpload.Root>
+  );
+};
--- src/components/posts/CreatePostForm.tsx
+++ src/components/posts/CreatePostForm.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { Button, Field, Flex, Input, Textarea } from '@chakra-ui/react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
+import { useUploadFiles } from '@/hooks/firebase/useUploadFiles';
 import { useCreatePost } from '@/hooks/posts/useCreatePost';
 import { PostPayload } from '@/models/posts.model';
+
+import { ImagesUploader } from '../ImagesUploader';
 
 const defaultValues = {
   title: '',
@@ -13,18 +17,49 @@ const defaultValues = {
 };
 
 export const CreatePostForm = () => {
+  // ..................................................
+  // Local States
+
+  const [files, setFiles] = useState<File[]>([]);
+
+  // ..................................................
+  // Hook Form
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<PostPayload>({ defaultValues });
 
+  // ..................................................
+  // API Hooks
+
+  const { mutateAsync: uploadFilesAsync, isPending: uploadingFilesIsLoading } =
+    useUploadFiles();
+
   const { mutate: createPost, isPending: creatingPostIsLoading } =
     useCreatePost();
 
-  const onSubmit = (payload: PostPayload) => {
-    createPost(payload);
+  // ..................................................
+  // Functions
+
+  const onSubmit = async (payload: PostPayload) => {
+    let imageUrls: string[] | null = null;
+
+    if (files.length) {
+      imageUrls = await uploadFilesAsync(files);
+    }
+
+    createPost({ ...payload, imageUrls });
   };
+
+  // ..................................................
+  // Variables
+
+  const loading = uploadingFilesIsLoading || creatingPostIsLoading;
+
+  // ..................................................
+  // Render
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -35,6 +70,13 @@ export const CreatePostForm = () => {
           <Field.ErrorText>{errors.title?.message}</Field.ErrorText>
         </Field.Root>
 
+        <ImagesUploader
+          maxW='xl'
+          alignItems='stretch'
+          maxFiles={10}
+          onFileChange={({ acceptedFiles }) => setFiles(acceptedFiles)}
+        />
+
         <Field.Root invalid={!!errors.description}>
           <Field.Label>Description</Field.Label>
           <Textarea
@@ -44,11 +86,7 @@ export const CreatePostForm = () => {
           <Field.ErrorText>{errors.description?.message}</Field.ErrorText>
         </Field.Root>
 
-        <Button
-          type='submit'
-          loading={creatingPostIsLoading}
-          disabled={creatingPostIsLoading}
-        >
+        <Button type='submit' loading={loading} disabled={loading}>
           Create
         </Button>
       </Flex>
